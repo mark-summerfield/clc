7945d1ae8eec470acfd7311d704bbc1a7aa51064
mark-summerfield <mark@qtrac.eu>
2022-10-14 13:33:07 +0100

    started rust version

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,6 +1,19 @@
 // Copyright © 2022 Mark Summerfield. All rights reserved.
 // License: GPLv3
 
+use clap::Parser;
+
 fn main() {
-    println!("Hello clc");
+    let config = Config::parse();
+    dbg!(config);
+}
+
+#[derive(Parser, Debug)]
+#[clap(version,
+       about = "Counts the lines in the code files for the languages \
+       processed.")]
+struct Config {
+    /// Sort by lines
+    #[arg(short, long)]
+    sortbylines: bool,
 }
